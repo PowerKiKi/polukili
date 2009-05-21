8e21562adb096b609165a906bca921d6d59740cf
PowerKiKi <adrien.crivelli@gmail.com>
2009-05-21 16:05:22 +0900

    ADD log timestamp

--- application/source/Logger.cpp
+++ application/source/Logger.cpp
@@ -15,11 +15,22 @@ namespace Polukili
       void Logger::log(const string& input, ...)
       {
          va_list args;
-         va_start( args, input );
+         va_start(args, input);
          char buffer[1024];
-         vsprintf( buffer, input.c_str(), args );
+         vsprintf(buffer, input.c_str(), args);
          string to_log( buffer );
+         
+         // Print time
+         const int l = 50;
+         char buf[l];
+         tm * pt;
+         time_t t;
+         time(&t);
+         pt = localtime(&t);
+         strftime(buf, l, "%Y-%m-%d %H:%M:%S ", pt);
+                  
          fstream fp(Constants::logFilename.data(), ios::out | ios::app);
+         fp << buf;
          fp << to_log << endl;
          fp.close();
       }
